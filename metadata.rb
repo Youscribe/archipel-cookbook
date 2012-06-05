4a505096c2ea7ffae21485e101e68a7f9930139a
Guilhem Lettron <guilhem@lettron.fr>
2012-06-05 12:33:06 +0200

    add depends and supports

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,8 @@ license          "Apache 2.0"
 description      "Installs/Configures archipel"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+supports         "ubuntu"
+
+depends          "python"
+depends          "build-essential"
